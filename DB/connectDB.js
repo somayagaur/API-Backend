cc8e09eca1b866572f00997072b6bba4d3658c3c
somayagaur <somyagour14@gmail.com>
2025-07-25 23:25:54 +0530

    update

--- DB/connectDB.js
+++ DB/connectDB.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const LocalDB = 'mongodb://localhost:27017/api_backend'; // Replace with your local MongoDB URI
+// const LocalDB = 'mongodb://localhost:27017/api_backend'; // Replace with your local MongoDB URI
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(LocalDB)
+    await mongoose.connect(process.env.Mongo_live_url)
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
